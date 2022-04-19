060aaff29a943fb15c7e3399b387f24eea16f146
HengistChan <hengistchan@outlook.com>
2022-04-19 22:37:10 +0800

    feat: 框架基本内容

--- packages/emoe-example/src/main.ts
+++ packages/emoe-example/src/main.ts
@@ -4,6 +4,10 @@ import App from "./App.vue";
 import "@/styles/main.scss";
 import "uno.css";
 
+import { createEditor } from "../../emoe-lib/src/index";
+
 const app = createApp(App);
+
+app.use(createEditor());
 
 app.mount("#app");
